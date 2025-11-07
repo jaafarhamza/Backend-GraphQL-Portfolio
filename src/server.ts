21ea60ba9798c57f73635b78558dd08743401c89
jaafarhamza <jaafar.hamza711@gmail.com>
2025-11-07 12:12:10 +0100

    add cors permission for the front

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,12 @@ import { PasswordHasher } from './infrastructure/security/PasswordHasher';
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Health check
