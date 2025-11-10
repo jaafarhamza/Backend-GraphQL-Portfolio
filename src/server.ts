800c37920b11209e5a71c8ebe62eaa9505f6878f
jaafarhamza <jaafar.hamza711@gmail.com>
2025-11-10 23:00:15 +0100

    cors modify

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: 'https://frontend-react-portfolio-sand.vercel.app/',
+    origin: 'https://frontend-react-portfolio-sand.vercel.app',
     credentials: true,
   })
 );
